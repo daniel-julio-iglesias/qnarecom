096af71f11174b6892edf66d7a9100e58fb3c0ed
daniel-julio-iglesias <daniel.julio.iglesias@gmaill.com>
2018-07-19 01:31:34 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,33 @@
+""" The MIT License (MIT)
+
+Copyright (c) 2017 Miguel Grinberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-you-will-never-guess#1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ qnarecom.py
@@ -0,0 +1,29 @@
+# Main application module
+
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+# microblog/
+#   venv/
+#   app/
+#    __init__.py
+#    routes.py
+#  microblog.py
+
+# Linux
+# export FLASK_APP=qnarecom.py
+# Windows
+# set FLASK_APP=qnarecom.py
+
+# flask run
+# python qnarecom.py
+
+# http://localhost:5000/
+# http://localhost:5000/index
+
+from app import app, db
+from app.models import User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
